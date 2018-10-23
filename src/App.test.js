61609497d2adcf3cfea1c9a2814504d94bfb516b
Evgeny Malashin <seedofpanic41@gmail.com>
2018-10-23 14:22:59 +0100

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {store} from "./store";
+import {Provider} from "react-redux";
+import {Server} from 'mock-socket';
+
+beforeEach(() => {
+    const mockServer = new Server('ws://battle.mobmind.ru:3003/ws');
+
+    mockServer.on('connection', ws => {});
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
