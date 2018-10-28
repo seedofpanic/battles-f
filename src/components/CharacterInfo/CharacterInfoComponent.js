fbf0537b3a99371badda987e9d75ed75f4152dc3
Evgeny <seedofpanic@mail.ru>
2018-10-28 17:23:33 +0000

    show effects

--- src/components/CharacterInfo/CharacterInfoComponent.js
+++ src/components/CharacterInfo/CharacterInfoComponent.js
@@ -7,6 +7,14 @@ export class CharacterInfoComponent extends React.Component {
         return <div>
             <div>{character.name}</div>
             <div>{character.health.toFixed(2)}/{character.healthMax.toFixed(2)}</div>
+            {character.effects ?
+                <ul>
+                    {character.effects.map((effect, index) =>
+                        <li key={index}>{effect.name}: {effect.ticks}</li>
+                    )}
+                </ul>
+                : ''
+            }
         </div>;
     }
 }
