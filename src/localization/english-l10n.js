ae0429c595e5777ec77d5706049fe0b0b1d40d6b
Oleg Grishaev <lubtip@gmail.com>
2018-11-04 15:19:46 +0300

    implemented localization

---
+++ src/localization/english-l10n.js
@@ -0,0 +1,7 @@
+export const EnglishResources = {
+    'start_battle': "Start Battle",
+    'start_ai_battle': "Start AI Battle",
+    'pre_alpha_v0.3': "Pre alpha v0.3"
+};
+
+Object.freeze(EnglishResources);
