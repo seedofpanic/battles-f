7f2aa3195ef54013385cedeedf1463ac51636ac3
Oleg Grishaev <lubtip@gmail.com>
2018-11-07 23:10:09 +0300

    localization is now can be accessed with singleton

--- src/components/CancelFight/CancelFightComponent.js
+++ src/components/CancelFight/CancelFightComponent.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import {cancelFightAction} from '../../store/actions/cancelFightAction';
-import {l10n_helper} from "../../localization/l10n-helper";
 import style from './CancelFightComponent.css';
+import l10n_instance from "../../localization/l10n-helper";
 
 export class CancelFightComponent extends React.Component {
-
-    l10n = new l10n_helper();
-
     render() {
-        return <a href="#" className={style.myCancelButton} onClick={() => this.cancelFight()}>{this.l10n.lang('cancel_fight')}</a>
+        return <a href="#" className={style.myCancelButton} onClick={() => this.cancelFight()}>{l10n_instance.lang('cancel_fight')}</a>
     }
 
     cancelFight() {
